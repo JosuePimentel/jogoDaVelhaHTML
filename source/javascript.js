70fce288478c19cb2d0b06489e75618af4f1f07e
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-07-19 13:27:06 -0300

    add button game over

--- source/javascript.js
+++ source/javascript.js
@@ -1,6 +1,8 @@
 const tables = document.querySelectorAll('div.jogo > span')
-let vez = 1
+let vez = Math.round(Math.random(0,1))
+const velha = vez + 9
 let over = 0
+let last = ''
 // par -> X
 // impar -> O
 
@@ -10,11 +12,18 @@ tables.forEach(ele => {
         const row = table.getAttribute('row')
         const column = table.getAttribute('column') 
         preencherGame(table)
-        // console.log(table.nextSibling)
         verLinha(row)
         verColuna(column)
+        verDiag()
+        const btnOver = document.querySelector('.over')
 
-        if(over) console.log('O jogo acabou')
+        if(over) {
+            overGame(last, row, column)
+            setInterval(() => {
+                btnOver.classList.add('show')
+            }, 5000)
+        }
+        else if(vez >= velha) console.log('O jogo deu velha')
     })
 })
 
@@ -35,7 +44,10 @@ function verLinha(row) {
     for (let column = 1; column <= 3; column++) {
         table.push(document.querySelector(`div.jogo > span[column="${column}"][row="${row}"]`).innerText)
     }
-    if(table[0] == table[1] && table[1] == table[2]) over++
+    if(table[0] == table[1] && table[1] == table[2]) {
+        over++
+        last = "Lin"
+    }
 }
 
 function verColuna(column) {
@@ -43,13 +55,34 @@ function verColuna(column) {
     for (let row = 1; row <= 3; row++) {
         table.push(document.querySelector(`div.jogo > span[row="${row}"][column="${column}"]`).innerText)
     }
-    if(table[0] == table[1] && table[1] == table[2]) over++
+    if(table[0] == table[1] && table[1] == table[2]) {
+        over++
+        last = "Col"
+    }
 }
 
-function verDiag(column, row) {
-    const table = new Array
+function verDiag() {
+    const tableDiag = new Array
+    const tableDiagInv = new Array
+    const tablleMiddle = document.querySelector(`div.jogo > span[row="${2}"][column="${2}"]`).innerText
     for (let row = 1; row <= 3; row++) {
-        table.push(document.querySelector(`div.jogo > span[row="${row}"][column="${column}"]`).innerText)
+        for (let column = 1; column <= 3; column++) {
+            if(row == column) tableDiag.push(document.querySelector(`div.jogo > span[row="${row}"][column="${column}"]`).innerText)
+            
+            if(row+column == 4) tableDiagInv.push(document.querySelector(`div.jogo > span[row="${row}"][column="${column}"]`).innerText)
+        }
     }
-    if(table[0] == table[1] && table[1] == table[2]) over++
+
+    if(tableDiag[0] == tableDiag[1] && tableDiag[1] == tableDiag[2] && tablleMiddle != '') {
+        over++
+        last = "Diag"
+    }
+    else if(tableDiagInv[0] == tableDiagInv[1] && tableDiagInv[1] == tableDiagInv[2] && tablleMiddle != '') {
+        over++
+        last = "DiagInv"
+    }
+}
+
+function overGame(last, row, column) {
+    
 }
